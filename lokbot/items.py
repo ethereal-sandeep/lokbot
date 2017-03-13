eca22127cab335c6c41969d9d6952c26adcb04ad
ethereal-sandeep <ethereal.sandeep@gmail.com>
2017-03-13 21:20:03 +0530

    model defined

--- lokbot/items.py
+++ lokbot/items.py
@@ -12,14 +12,22 @@ import scrapy
 
 class LokbotItem(scrapy.Item):
     # product model here
+    product_title = scrapy.Field()
+    product_price = scrapy.Field()  # price in dollar
+    product_shipping_info = scrapy.Field()
+    product_company = scrapy.Field()
     pass
 
 
 class LokbotTotalItem(scrapy.Item):
     # total result count model here
+    keyword = scrapy.Field()
+    total_product_count = scrapy.Field()
     pass
 
 
 class Status(scrapy.Item):
     # common status message here
+    msg_text = scrapy.Field()
+    msg_type = scrapy.Field()
     pass
